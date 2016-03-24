09487c80dd9061d4313ff8c0456f0e9933d1ec3c
Ashrith Sheshan <ashrith.sheshan@nutanix.com>
2016-03-25 04:11:20 +0530

    Dynamic Naviagation panel with complete navigation events handled

--- assets/js/load-content.js
+++ assets/js/load-content.js
@@ -5,9 +5,38 @@ function onYouTubeIframeAPIReady() {
         loadContentBody(module.videoId, module.timerData);
     }
 
+    function getNavigation(data) {
+        var html = '<li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
+        for (key in data) {
+            var mod = data[key];
+            html += '<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' + mod.name + '</a><ul class="dropdown-menu">';
+            for (j in mod.children) {
+                var ch = mod.children[j];
+                html += '<li><a class="module" module="' + ch.module + '" href="#">' + ch.name + '</a></li>'
+            }
+            html += '</ul></li>'
+        }
+        html += '<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+        $('nav ul.pagination').html(html);
+    }
+
     $.getJSON("/assets/js/content.json", function(data) {
         console.log(data);
-        var firstModule = data.modules[0];
+
+        //Get the navigation
+        getNavigation(data.navigation);
+
+        var firstModule = data.modules["1"];
         loadModule(firstModule);
+
+        $('nav ul.pagination li a.module').click(function(elem) {
+            var module = $(elem.target).attr('module');
+            if (!(module in data.modules)){
+            	alert('Module Not Implemented yet.');
+            	return
+            }
+            loadModule(data.modules[module])
+        });
+
     });
 }
